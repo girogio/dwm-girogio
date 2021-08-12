ab462bcf7c09c723d9204cee7ed190e6d5ddaa63
Giorgio Grigolo <giorgiogrigolo03@gmail.com>
2021-08-12 12:30:50 +0200

    Fixed bit masks for tagmask

--- config.h
+++ config.h
@@ -33,8 +33,9 @@ static const char *tags[] = { "1", "2", "3", "4", "5" };
 static const Rule rules[] = {
 
     /* class      	     instance    title    tags mask     isfloating   CenterThisWindow?     monitor */
-	{ "st",              NULL,       NULL,    0 << 2,            0,     	     1,	    	           -1 },
+	{ "st",              NULL,       NULL,    1,                 0,     	     1,	    	           -1 },
 	{ "discord",         NULL,       NULL,    0,                 0,     	     1,	    	           -1 },
+	{ "firefox",         NULL,       NULL,    1 << 1,            1,     	     0,	    	           -1 },
 
 
 };
